54e3069f5a1916703f91664a74e5acb182f2b533
NiU848 <108462119+NiU848@users.noreply.github.com>
2025-12-04 13:02:22 +0200

    fixed some bugs with presume-ack

--- drivers/net/can/spi/mcp251xfd/mcp251xfd-tef.c
+++ drivers/net/can/spi/mcp251xfd/mcp251xfd-tef.c
@@ -311,17 +311,6 @@ int mcp251xfd_handle_txatif(struct mcp251xfd_priv *priv)
 	u32 tef_tail, val;
 	u32 intf, fifosta, tefsta, fifocon, tefcon;
 
-	can_free_echo_skb(priv->ndev, tx_head, &frame_len);
-	regmap_read(priv->map_reg, MCP251XFD_REG_INT, &intf);
-    regmap_read(priv->map_reg, MCP251XFD_REG_TEFSTA, &tefsta);
-
-    netdev_info(priv->ndev, "Start INTF=0x%08x TEFSTA=0x%08x\n", intf, tefsta);
-
-    /* Dump FIFO status for all TX/RX FIFOs */
-    regmap_read(priv->map_reg, MCP251XFD_REG_FIFOSTA(fifo_nr), &fifosta);
-	regmap_read(priv->map_reg, MCP251XFD_REG_FIFOCON(fifo_nr), &fifocon);
-    netdev_info(priv->ndev, "FIFO[%d] STA=0x%08x con=0x%08x\n", fifo_nr, fifosta, fifocon);
-
 	err = regmap_update_bits(priv->map_reg,
 							MCP251XFD_REG_FIFOSTA(fifo_nr),
 							MCP251XFD_REG_FIFOSTA_TXATIF,
@@ -331,33 +320,27 @@ int mcp251xfd_handle_txatif(struct mcp251xfd_priv *priv)
 		return err;
 	}
 	tef_tail = mcp251xfd_get_tef_tail(priv);
-	// can_rx_offload_get_echo_skb(&priv->offload,
-	// 				tef_tail, 0,
-	// 				&frame_len);
-	netdev_info(priv->ndev, "frame len=%i tef_tail=%i, tx_tail=%i\n", frame_len, chip_tef_tail, chip_tx_tail);
+	can_rx_offload_get_echo_skb(&priv->offload,
+					tef_tail, 0,
+					&frame_len);
 	netdev_completed_queue(priv->ndev, 1, frame_len);
 	mcp251xfd_ecc_tefif_successful(priv);
 	if (mcp251xfd_get_tx_free(priv->tx)) {
 			/* Make sure that anybody stopping the queue after
 			* this sees the new tx_ring->tail.
 			*/
-			netdev_info(priv->ndev, "in\n");
 			smp_mb();
 			netif_wake_queue(priv->ndev);
 		}
 	if (priv->can.ctrlmode == CAN_CTRLMODE_PRESUME_ACK)
 	{
-		// priv->tef->head = tx_ring->head;
-		// priv->tef->tail = tx_ring->head;
-
-		// tx_ring->tail = tx_ring->head;
 		regmap_update_bits(priv->map_reg,
 						MCP251XFD_REG_FIFOCON(priv->tx->fifo_nr),
 						MCP251XFD_REG_FIFOCON_TXAT_MASK | MCP251XFD_REG_FIFOCON_TXREQ,
 						FIELD_PREP(MCP251XFD_REG_FIFOCON_TXAT_MASK,
-				 		MCP251XFD_REG_FIFOCON_TXAT_UNLIMITED) | MCP251XFD_REG_FIFOCON_TXREQ);
-		regmap_read(priv->map_reg, MCP251XFD_REG_FIFOCON(fifo_nr), &fifocon);
-    	netdev_info(priv->ndev, "WR=0x%08x-0x%08x con=0x%08x\n", MCP251XFD_REG_FIFOCON_TXAT_MASK | MCP251XFD_REG_FIFOCON_TXREQ, MCP251XFD_REG_FIFOCON_TXAT_UNLIMITED | MCP251XFD_REG_FIFOCON_TXREQ, fifocon);
+				 		MCP251XFD_REG_FIFOCON_TXAT_UNLIMITED) | MCP251XFD_REG_FIFOCON_TXREQ
+		);
+		can_free_echo_skb(priv->ndev, tx_head, &frame_len); // don't know why it's needed here but it prevents SKB occupied bug
 	}
 	else{
 		regmap_update_bits(priv->map_reg,
@@ -378,19 +361,8 @@ int mcp251xfd_handle_txatif(struct mcp251xfd_priv *priv)
 		priv->tef->tail = 0;
 
 		tx_ring->head = 0;
-		tx_ring->tail = 0;
-
+		tx_ring->tail = 0;	
 	}
-
-	regmap_read(priv->map_reg, MCP251XFD_REG_INT, &intf);
-    regmap_read(priv->map_reg, MCP251XFD_REG_TEFSTA, &tefsta);
-
-    netdev_info(priv->ndev, "END INTF=0x%08x TEFSTA=0x%08x\n", intf, tefsta);
-
-    /* Dump FIFO status for all TX/RX FIFOs */
-    regmap_read(priv->map_reg, MCP251XFD_REG_FIFOSTA(fifo_nr), &fifosta);
-	regmap_read(priv->map_reg, MCP251XFD_REG_FIFOCON(fifo_nr), &fifocon);
-    netdev_info(priv->ndev, "FIFO[%d] STA=0x%08x CON=0x%08x\n\n ", fifo_nr, fifosta, fifocon);
-
+	can_free_echo_skb(priv->ndev, tx_head, &frame_len);
     return 0;
 }
