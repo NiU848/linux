8bb6a95f672efe5b3d256f2dc9e3abbdd7a4e91a
NiU848 <108462119+NiU848@users.noreply.github.com>
2026-01-27 11:13:06 +0200

    presume-ack stability

--- drivers/net/can/spi/mcp251xfd/mcp251xfd-tx.c
+++ drivers/net/can/spi/mcp251xfd/mcp251xfd-tx.c
@@ -12,7 +12,7 @@
 // Copyright (c) 2019 Martin Sperl <kernel@martin.sperl.org>
 //
 
-#include <linux/unaligned.h>
+#include <asm/unaligned.h>
 #include <linux/bitfield.h>
 
 #include "mcp251xfd.h"
@@ -131,38 +131,6 @@ mcp251xfd_tx_obj_from_skb(const struct mcp251xfd_priv *priv,
 	tx_obj->xfer[0].len = len;
 }
 
-static void mcp251xfd_tx_failure_drop(const struct mcp251xfd_priv *priv,
-				      struct mcp251xfd_tx_ring *tx_ring,
-				      int err)
-{
-	struct net_device *ndev = priv->ndev;
-	struct net_device_stats *stats = &ndev->stats;
-	unsigned int frame_len = 0;
-	u8 tx_head;
-
-	tx_ring->head--;
-	stats->tx_dropped++;
-	tx_head = mcp251xfd_get_tx_head(tx_ring);
-	can_free_echo_skb(ndev, tx_head, &frame_len);
-	netdev_completed_queue(ndev, 1, frame_len);
-	netif_wake_queue(ndev);
-
-	if (net_ratelimit())
-		netdev_err(priv->ndev, "ERROR in %s: %d\n", __func__, err);
-}
-
-void mcp251xfd_tx_obj_write_sync(struct work_struct *work)
-{
-	struct mcp251xfd_priv *priv = container_of(work, struct mcp251xfd_priv,
-						   tx_work);
-	struct mcp251xfd_tx_obj *tx_obj = priv->tx_work_obj;
-	struct mcp251xfd_tx_ring *tx_ring = priv->tx;
-	int err;
-
-	err = spi_sync(priv->spi, &tx_obj->msg);
-	if (err)
-		mcp251xfd_tx_failure_drop(priv, tx_ring, err);
-}
 
 static int mcp251xfd_tx_obj_write(const struct mcp251xfd_priv *priv,
 				  struct mcp251xfd_tx_obj *tx_obj)
@@ -195,11 +163,6 @@ static bool mcp251xfd_tx_busy(const struct mcp251xfd_priv *priv,
 	return false;
 }
 
-static bool mcp251xfd_work_busy(struct work_struct *work)
-{
-	return work_busy(work);
-}
-
 netdev_tx_t mcp251xfd_start_xmit(struct sk_buff *skb,
 				 struct net_device *ndev)
 {
@@ -209,7 +172,7 @@ netdev_tx_t mcp251xfd_start_xmit(struct sk_buff *skb,
 	unsigned int frame_len;
 	u8 tx_head;
 	int err;
-	u32 intf, fifosta, tefsta, tefcon, fifocon;
+	u32 /*intf, fifosta, tefsta,*/ tefcon, fifocon;
 	const u8 fifo_nr = tx_ring->fifo_nr;
 
 	if (can_dev_dropped_skb(ndev, skb))
@@ -220,14 +183,16 @@ netdev_tx_t mcp251xfd_start_xmit(struct sk_buff *skb,
 	{
 		return NETDEV_TX_BUSY;
 	}
+	//netdev_info(priv->ndev, "XMIT");
 	if (priv->can.ctrlmode & CAN_CTRLMODE_PRESUME_ACK)
 	{
 		regmap_update_bits(priv->map_reg,
 						MCP251XFD_REG_FIFOCON(priv->tx->fifo_nr),
 						MCP251XFD_REG_FIFOCON_TXAT_MASK | MCP251XFD_REG_FIFOCON_FRESET,
 						FIELD_PREP(MCP251XFD_REG_FIFOCON_TXAT_MASK,
-				 	 			MCP251XFD_REG_FIFOCON_TXAT_ONE_SHOT) |
-								MCP251XFD_REG_FIFOCON_FRESET);
+				 	 		MCP251XFD_REG_FIFOCON_TXAT_ONE_SHOT) |
+						MCP251XFD_REG_FIFOCON_FRESET
+		);
 		regmap_update_bits(priv->map_reg,
 					MCP251XFD_REG_TEFCON,
 					MCP251XFD_REG_TEFCON_FRESET,
@@ -240,6 +205,10 @@ netdev_tx_t mcp251xfd_start_xmit(struct sk_buff *skb,
 		do{
 			regmap_read(priv->map_reg, MCP251XFD_REG_TEFCON, &tefcon);
 		}while(tefcon & MCP251XFD_REG_TEFCON_FRESET);
+		
+		// regmap_update_bits(priv->map_reg, MCP251XFD_REG_TEFCON,
+		// 								MCP251XFD_REG_TEFCON_TEFNEIE,
+		// 								MCP251XFD_REG_TEFCON_TEFNEIE);
 
 		priv->tef->head = 0;
 		priv->tef->tail = 0;
@@ -256,23 +225,33 @@ netdev_tx_t mcp251xfd_start_xmit(struct sk_buff *skb,
 		netif_stop_queue(ndev);
 
 	frame_len = can_skb_get_frame_len(skb);
+	
+	// netdev_info(ndev,
+	// 	"TX head=%u tail=%u free=%u frame_len=%u\n",
+	// 	tx_ring->head,
+	// 	tx_ring->tail,
+	// 	mcp251xfd_get_tx_free(tx_ring),
+	// 	frame_len);
 
 	err = can_put_echo_skb(skb, ndev, tx_head, frame_len);
 	if (!err)
 	{
 		netdev_sent_queue(priv->ndev, frame_len);
 	}
+	// regmap_read(priv->map_reg, MCP251XFD_REG_INT, &intf);
+    // regmap_read(priv->map_reg, MCP251XFD_REG_TEFSTA, &tefsta);
+	// regmap_read(priv->map_reg, MCP251XFD_REG_FIFOSTA(fifo_nr), &fifosta);
+    // netdev_info(priv->ndev, "After send INTF=0x%08x TEFSTA=0x%08x\n", intf, tefsta);
+    // netdev_info(priv->ndev, "FIFO[%d] STA=0x%08x\n", fifo_nr, fifosta);
 
 	err = mcp251xfd_tx_obj_write(priv, tx_obj);
-	if (err == -EBUSY)
-	{
-		netif_stop_queue(ndev);
-		priv->tx_work_obj = tx_obj;
-		queue_work(priv->wq, &priv->tx_work);
-	}
-	else if(err)
-	{
-		mcp251xfd_tx_failure_drop(priv, tx_ring, err);
-	}
+	if (err)
+		goto out_err;
+
 	return NETDEV_TX_OK;
+
+ out_err:
+	netdev_err(priv->ndev, "ERROR in %s: %d\n", __func__, err);
+
+	return NETDEV_TX_OK;;
 }
